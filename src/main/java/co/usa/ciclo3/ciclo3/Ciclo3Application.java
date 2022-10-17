cc79537c44609f5fc264060cf3b82b385924aa85
lmnietoa <lmnietoa@gmail.com>
2022-10-17 18:39:40 -0500

    Quinto Commit

--- src/main/java/co/usa/ciclo3/ciclo3/Ciclo3Application.java
+++ src/main/java/co/usa/ciclo3/ciclo3/Ciclo3Application.java
@@ -3,11 +3,13 @@ package co.usa.ciclo3.ciclo3;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 import javax.persistence.Entity;
 
 @EntityScan(basePackages = {"co.usa.ciclo3.ciclo3.model"})
 @SpringBootApplication
+@ComponentScan(basePackages= {"com.ejemplo"})
 public class Ciclo3Application {
 
 	public static void main(String[] args) {
